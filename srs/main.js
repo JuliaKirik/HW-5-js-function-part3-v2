43600fd027c9abb7d4986d156dfffa376712cab7
JuliaKirik <juliakirichok9878@gmail.com>
2022-04-12 17:17:54 +0300

    changed

--- srs/main.js
+++ srs/main.js
@@ -1,30 +1,26 @@
 console.log ("...........#4 Упрощенный вариант:")
-
-function ab (A, B) {
-    if (A > B) {
+function ab (a, b) {
+    if (a > b) {
         return
     }
-    console.log(A);
-    ab(A + 1, B);
+    console.log (a);
+    ab(a + 1, b);
 }
 ab(2, 10)
-
 console.log ("...........#4 Усложненный вариант:")
-function abc (A, B) {
-    console.log(A);
-    if (A === B) {
+function abc (a, b) {
+    console.log (a);
+    if (a === b) {
         return
-    } else if(A < B ) { 
-        abc(A + 1, B);
+    } else if (a < b) { 
+        abc(a + 1, b);
     } else {
-        abc(A - 1, B);
+        abc(a - 1, b);
     }   
 }
 abc(2, 15)
-
 console.log ("...........#5 (a,b) Реализовать отображение времени на экране:")
-
-function clock(){
+function clock() {
     const date = new Date(),
         hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
         minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
